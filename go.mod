d578f545bae57fad43e82021cb5cd890b9e778db
Nick Satterly <nfsatterly@gmail.com>
2019-06-16 01:13:05 +0200

    Update Alerta handler to Sensu Go (#8)

---
+++ go.mod
@@ -0,0 +1,25 @@
+module sensu-alerta-handler
+
+go 1.12
+
+require (
+	github.com/coreos/etcd v3.3.10+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/echlebek/crock v1.0.1 // indirect
+	github.com/echlebek/timeproxy v1.0.0 // indirect
+	github.com/gogo/protobuf v1.1.1 // indirect
+	github.com/google/gofuzz v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/json-iterator/go v0.0.0-20171115153421-f7279a603ede // indirect
+	github.com/robertkrimen/otto v0.0.0-20180617131154-15f95af6e78d // indirect
+	github.com/robfig/cron v0.0.0-20171101201047-2315d5715e36 // indirect
+	github.com/sensu/sensu-go v0.0.0-20181130214924-2daf9d442dee
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/stretchr/testify v1.3.0
+	golang.org/x/net v0.0.0-20181201002055-351d144fa1fc // indirect
+	golang.org/x/sys v0.0.0-20181206074257-70b957f3b65e // indirect
+	google.golang.org/genproto v0.0.0-20181202183823-bd91e49a0898 // indirect
+	google.golang.org/grpc v1.17.0 // indirect
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+)
---
+++ main_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/sensu/sensu-go/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+
+func TestSendAlert(t *testing.T) {
+	assert := assert.New(t)
+	event := types.FixtureEvent("entity1", "check1")
+
+	var apiStub = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, _ := ioutil.ReadAll(r.Body)
+		expectedBody := `{"resource":"entity1","event":"check1","environment":"default","severity":"normal","correlate":null,"status":"","service":["Sensu"],"group":"default","value":"","text":"","tags":null,"attributes":null,"origin":"sensu-go/`
+		assert.Contains(string(body), expectedBody)
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(`{"status": "ok"}`))
+		require.NoError(t, err)
+	}))
+
+	config.AlertaEndpoint.Value = apiStub.URL
+	config.AlertaApiKey.Value = "demo-key"
+	err := sendAlert(event)
+	assert.NoError(err)
+}
+
+func TestMain(t *testing.T) {
+	assert := assert.New(t)
+	file, _ := ioutil.TempFile(os.TempDir(), "sensu-handler-alerta-")
+	defer func() {
+		_ = os.Remove(file.Name())
+	}()
+
+	event := types.FixtureEvent("entity1", "check1")
+	eventJSON, _ := json.Marshal(event)
+	_, err := file.WriteString(string(eventJSON))
+	require.NoError(t, err)
+	require.NoError(t, file.Sync())
+	_, err = file.Seek(0, 0)
+	require.NoError(t, err)
+	stdin = file
+	requestReceived := false
+
+	var apiStub = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestReceived = true
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(`{"status": "ok"}`))
+		require.NoError(t, err)
+	}))
+
+	oldArgs := os.Args
+	os.Args = []string{"alerta", "--endpoint-url", apiStub.URL}
+	defer func() { os.Args = oldArgs }()
+
+	main()
+	assert.True(requestReceived)
+}
